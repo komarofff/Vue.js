5054af2b5d8fce0624dda65c51900e2a63356747
Anatoliy Komarov <admin@komaroff.biz>
2022-01-06 11:12:37 +0300

    Update GetPhotos.vue

--- test/src/components/GetPhotos.vue
+++ test/src/components/GetPhotos.vue
@@ -1,32 +1,28 @@
 <template>
 
-  <div class="w-7/12 mx-auto grid grid-cols-3 gap-20 items-end mb-6">
+  <div class="w-full lg:w-8/12 mx-auto grid grid-cols-1 md:grid-cols-3 gap-10 md:gap-20 items-end mb-6">
     <div class="flex flex-col justify-start items-center w-full ">
-      <label class="mb-2 text-lg">Number of images:</label>
-<!--      <select v-model="amount"-->
-<!--              class="border border-gray-200 text-xl rounded-lg w-full p-2 h-12 focus:border-4 focus:border-blue-200 outline-none">-->
-<!--        <option v-for="i in 50">{{ i }}</option>-->
-<!--      </select>-->
+      <p class="mb-2 text-lg">Number of images:</p>
       <input type="number" min="1" max="50" v-model="amount"
              class="border border-gray-200 text-xl rounded-lg w-full p-2 h-12 focus:border-4 focus:border-blue-200 outline-none">
     </div>
     <div class="flex flex-col justify-start items-center w-full ">
-      <label class="mb-2 text-lg">Select date:</label>
+      <p class="mb-2 text-lg">Select date:</p>
       <input type="date" v-model="date"
              class="border border-gray-200 text-lg rounded-lg w-full p-2 h-12  focus:border-4 focus:border-blue-200 outline-none">
     </div>
-    <div>
+    <div class="flex justify-center">
       <button @click="getData" class="bg-blue-600 text-white text-lg rounded-lg py-2 px-8 self-center h-12"> Submit
       </button>
     </div>
 
   </div>
 
-  <div v-for="item in dataFromServer" class="min-w-screen  grid grid-cols-4 gap-20">
+  <div v-for="item in dataFromServer" class="min-w-screen  grid grid-cols-1 md:grid-cols-4 gap-20">
 
     <div v-for="photo in item" v-if="item.length > 0"
          class="flex flex-col justify-start items-center border-2 border-gray-200 bg-white self-straight rounded">
-      <img :src="photo.img_src" class="w-full">
+      <img :src="photo.img_src" class="w-full" >
       <p class="text-xl">{{ photo.id }}</p>
       <p>{{ photo.earth_date }}</p>
     </div>
